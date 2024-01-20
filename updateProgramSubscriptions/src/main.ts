5b521c82438cc10b05617b0693fd7add3bb08f8e
coreyauger <coreyauger@gmail.com>
2024-01-20 17:19:40 +0800

    update

--- updateProgramSubscriptions/src/main.ts
+++ updateProgramSubscriptions/src/main.ts
@@ -1,4 +1,16 @@
-import { Client } from 'node-appwrite';
+import { Client, Account, Databases, Query } from 'node-appwrite';
+
+function throwIfMissing(obj: any, keys: string[]): void {
+  const missing: string[] = [];
+  for (let key of keys) {
+    if (!(key in obj) || !obj[key]) {
+      missing.push(key);
+    }
+  }
+  if (missing.length > 0) {
+    throw new Error(`Missing required fields: ${missing.join(", ")}`);
+  }
+}
 
 type Context = {
   req: any;
@@ -10,31 +22,82 @@ type Context = {
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }: Context) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  throwIfMissing(process.env, [
+    'APPWRITE_API_KEY',
+    'APPWRITE_FUNCTION_PROJECT_ID',
+    'APPWRITE_DATABASE_ID',
+  ]);
+    try{
+       // The `req` object contains the request data
+       if (req.method === 'GET') {
+        // Send a response with the res object helpers
+        // `res.send()` dispatches a string back to the client
+        return res.error('expected POST, DELETE');
+      }
+      const jwtToken = req.body.jwtToken;
+      if(!jwtToken)throw new Error("No JWT token in request body");
+      const programId = req.body.programId;
+      if(!programId)throw new Error("No programId in request body");
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+      const userClient = new Client()
+        .setEndpoint('https://cloud.appwrite.io/v1')
+        .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID!)
+        .setJWT(jwtToken);
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+      const userAccount = new Account(userClient);
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+      const userId = (await userAccount.get()).$id;
+      if(!userId)throw new Error("No user found from JWT token");
+      log(`got userId: ${userId}`);
+
+      const client = new Client()
+          .setEndpoint('https://cloud.appwrite.io/v1')
+          .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID!)
+          .setKey(process.env.APPWRITE_API_KEY!);
+
+      const db = new Databases(client);
+
+      const {documents} = await db.listDocuments(
+        process.env.APPWRITE_DATABASE_ID!,
+        "subscription",
+        [
+          Query.equal("user_key",userId )
+        ]
+      );
+      log(`documents: ${documents}`);
+      const program = await db.getDocument(
+        process.env.APPWRITE_DATABASE_ID!,
+        "program",
+        programId
+      );
+      log(`program: ${program}`)
+      if(documents.length){                
+        log(`update`);
+        /*db.updateDocument(
+          process.env.APPWRITE_DATABASE_ID!,
+        "subscription",
+        {
+          ...document.
+        }
+        [
+          Query.equal("user_key",userId )
+        ]
+        )*/
+      }else{
+        log(`create`);
+      }
+
+     
+
+      // `res.json()` is a handy helper for sending JSON
+      return res.json({
+        motto: 'Build like a team of hundreds_',
+        learn: 'https://appwrite.io/docs',
+        connect: 'https://appwrite.io/discord',
+        getInspired: 'https://builtwith.appwrite.io',
+      });
+  }catch(e:any) {
+    error(e);
+    res.error(e.message);
   }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
 };
