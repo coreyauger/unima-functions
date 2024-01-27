f24c07a2d099a0994ab7451a9d3b9c7f893ecaab
coreyauger <coreyauger@gmail.com>
2024-01-27 18:58:04 +0800

    getSessionVideo

--- getSessionVideo/src/main.ts
+++ getSessionVideo/src/main.ts
@@ -57,7 +57,7 @@ export default async ({ req, res, log, error }: Context) => {
 
       // UNIX timestamp when the vidoe link will expire (default to 120 minutes)
       const timestamp = (Date.now() + (120 * 60 * 1000)) / 1000;  // UNIX POSIX is in seconds not ms.
-      const hash = createHash(process.env.BUNNY_API_KEY! + videoId + timestamp);
+      const hash = sha256(process.env.BUNNY_API_KEY! + videoId + timestamp);
       return res.json({
         hash,
         timestamp,
