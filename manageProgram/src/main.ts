0f1ccbca03d8a03212ef3f2fdadfe1ba2b747911
coreyauger <coreyauger@gmail.com>
2024-02-09 18:06:36 +0800

    feat: manage programs

---
+++ manageProgram/src/main.ts
@@ -0,0 +1,129 @@
+import { Client, Account, Databases, Permission, ID, Role } from 'node-appwrite';
+import { connect } from 'getstream';
+
+function throwIfMissing(obj: any, keys: string[]): void {
+  const missing: string[] = [];
+  for (let key of keys) {
+    if (!(key in obj) || !obj[key]) {
+      missing.push(key);
+    }
+  }
+  if (missing.length > 0) {
+    throw new Error(`Missing required fields: ${missing.join(", ")}`);
+  }
+}
+
+type Context = {
+  req: any;
+  res: any;
+  log: (msg: any) => void;
+  error: (msg: any) => void;
+};
+
+// This is your Appwrite function
+// It's executed each time we get a request
+export default async ({ req, res, log, error }: Context) => {
+  throwIfMissing(process.env, [
+    'APPWRITE_API_KEY',
+    'APPWRITE_FUNCTION_PROJECT_ID',
+    'APPWRITE_DATABASE_ID',
+    'STREAM_API_KEY',
+    'STREAM_API_SECRET',
+    'STREAM_APP_ID',
+  ]);
+    try{
+       // The `req` object contains the request data
+       if (req.method === 'GET') {
+        // Send a response with the res object helpers
+        // `res.send()` dispatches a string back to the client
+        return res.error('expected POST, DELETE');
+      }
+      log(req.body);
+      const jsonPayload = JSON.parse(req.body);
+      const jwtToken = jsonPayload.jwtToken;      
+      if(!jwtToken)throw new Error("No JWT token in request body");          
+      
+      const userClient = new Client()
+        .setEndpoint('https://cloud.appwrite.io/v1')
+        .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID!)
+        .setJWT(jwtToken);
+
+      const userAccount = new Account(userClient);
+
+      const userId = (await userAccount.get()).$id;
+      if(!userId)throw new Error("No user found from JWT token");
+      log(`got userId: ${userId}`);
+      log(`update program: ${JSON.stringify(jsonPayload)}`);
+
+      const client = new Client()
+          .setEndpoint('https://cloud.appwrite.io/v1')
+          .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID!)
+          .setKey(process.env.APPWRITE_API_KEY!);
+
+      const db = new Databases(client);
+      const program = await db.getDocument(process.env.APPWRITE_DATABASE_ID!,
+        "program",
+        jsonPayload.$id
+        ).catch((r) => undefined);
+      if(program?.$id){
+        // TODO: check if the this is the instructor        
+        const doc = await db.updateDocument(process.env.APPWRITE_DATABASE_ID!,
+          "program",
+          program?.$id, {
+            ...jsonPayload,
+            profile: (program as any).profile.$id, // Don't allow a profile id change
+          });
+          await db.updateDocument(process.env.APPWRITE_DATABASE_ID!,
+            "profile",
+            (program as any).profile.$id, {
+              ...(program as any).profile
+            });          
+          return res.json(doc);               
+      } else {
+        const profileId = ID.unique();
+        await db.createDocument(
+          process.env.APPWRITE_DATABASE_ID!,
+          "profile",
+          profileId,
+          (program as any).profile,
+          [
+            Permission.read(Role.users()),        
+            Permission.update(Role.team("admin")),
+            Permission.delete(Role.team("admin")),
+            Permission.update(Role.user(userId)),            
+        ]);
+        await db.createDocument(
+          process.env.APPWRITE_DATABASE_ID!,
+          "program",
+          profileId,
+          {
+            ...program,
+            profile: profileId,
+          },
+          [
+            Permission.read(Role.users()),        
+            Permission.update(Role.team("admin")),
+            Permission.delete(Role.team("admin")),
+            Permission.update(Role.user(userId)),            
+        ]
+        );
+        const streamClient = connect(process.env.STREAM_API_KEY!, process.env.STREAM_API_SECRET!, process.env.STREAM_APP_ID!);
+        const user = await streamClient.user(profileId).getOrCreate((program as any).profile);
+        await user.update((program as any).profile);
+        // we want to follow our own feed in the timeline..
+        const timelineFeed = streamClient.feed('timeline', profileId);
+        timelineFeed.follow("user", profileId);
+        log("We have a stream result");
+        //log(`createResult data: ${createResult.data}`);  
+        const doc = await db.getDocument(
+          process.env.APPWRITE_DATABASE_ID!,
+          "program",
+          profileId
+        );
+        return res.json(doc);
+      }
+  }catch(e:any) {
+    error(e);
+    throw e;
+  }
+};
