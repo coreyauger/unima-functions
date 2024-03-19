17c7e13a65c951e3b8fff46f0ffabdd816a255a8
coreyauger <coreyauger@gmail.com>
2024-03-19 10:27:50 +0800

    change back

--- streamFunctions/src/main.ts
+++ streamFunctions/src/main.ts
@@ -105,13 +105,18 @@ export default async ({ req, res, log, error }: Context) => {
         const ret = await feed.follow("user", profileId);
         // send a notificaiton
         const notification = streamClient.feed('notification', profileId);
-        const streamUser = streamClient.user(userId);
         const db = new Databases(client);
-        const followProfile = await db.getDocument(process.env.APPWRITE_DATABASE_ID!,
-          "profile",
-          profileId);
-        const activityData = {actor: streamUser, verb: 'follow', object: `${profileId}`, time: new Date().toISOString(), extra_data: followProfile} 
-        const activityResponse = await notification.addActivity(activityData);
+        const userProfile = await db.getDocument(process.env.APPWRITE_DATABASE_ID!, "profile",userId);
+        const activityData = {
+          actor: {
+            data: userProfile,
+            id: userId,
+          }, 
+          verb: 'follow', 
+          object: `${profileId}`, 
+          time: new Date().toISOString()
+        }; 
+        const activityResponse = await notification.addActivity(activityData as any);
         log("notification response: " + JSON.stringify(activityResponse));
         return res.json({
           ret
